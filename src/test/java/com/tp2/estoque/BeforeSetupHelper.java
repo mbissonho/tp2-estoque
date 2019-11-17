d052090bcd747d437ef72168ab510a5dd9e5835a
Mateus A. Bissonho <mateus_bissonho@live.com>
2019-11-17 00:47:00 -0300

    Configurando Selenium Server para testes do Cucumber rodarem no ambiente remoto

--- src/test/java/com/tp2/estoque/BeforeSetupHelper.java
+++ src/test/java/com/tp2/estoque/BeforeSetupHelper.java
@@ -2,14 +2,19 @@ package com.tp2.estoque;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BeforeSetupHelper {
 
 	private static WebDriver firefoxWebdriver = null;
 	
 	public static void setUpFirefoxDriver() {
+		
+		FirefoxOptions options = new FirefoxOptions();
+		options.setHeadless(true);
+		
 		System.setProperty("webdriver.gecko.driver","src/test/resources/geckodriver");
-		BeforeSetupHelper.firefoxWebdriver = (WebDriver) new FirefoxDriver();
+		BeforeSetupHelper.firefoxWebdriver = (WebDriver) new FirefoxDriver(options);
 	}
 	
 	public static WebDriver getFirefoxWebdriver() {
