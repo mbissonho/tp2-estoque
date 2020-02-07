8775dbcfb7b656aaa06efc30b28558ea7fa8260c
Mateus A. Bissonho <mateus_bissonho@live.com>
2020-02-07 15:20:11 -0300

    Adicionada configuração de encoding do thymeleaf

---
+++ src/main/java/com/tp2/estoque/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.tp2.estoque.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    @Description("Thymeleaf view resolver")
+    public ViewResolver viewResolver() {
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }
+
+}
